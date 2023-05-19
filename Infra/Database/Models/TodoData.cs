16d00a9d960be7666ee5f06706c91f123d2ee5a8
Note45 <antonion.2048@gmail.com>
2023-05-18 22:08:16 -0300

    chore: setup project

---
+++ Infra/Database/Models/TodoData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace TodoListAPI.Infra.Database.Models
+{
+	public class TodoData
+	{
+		public string Id { get; set; }
+		public string Description { get; set; }
+		public string CreatedAt { get; set; }
+    }
+}
+
---
+++ Infra/Repositories/TodoListRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace TodoListAPI.Infra
+{
+	public class ToDoRepository
+	{
+		private List<object> _todoList = new List<object>();
+
+		public ToDoRepository()
+		{
+		}
+
+		public object GetTodo() 
+		{
+			return new Object();
+		}
+	}
+}
+
