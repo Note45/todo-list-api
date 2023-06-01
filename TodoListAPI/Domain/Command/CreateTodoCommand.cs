f24ed0f26f569ba3861120dc2d2af67f73367d2c
Note45 <antonion.2048@gmail.com>
2023-05-31 22:19:19 -0300

    feat: refactor CreateTodoCommand to add optional fields

--- TodoListAPI/Domain/Command/CreateTodoCommand.cs
+++ TodoListAPI/Domain/Command/CreateTodoCommand.cs
@@ -6,11 +6,11 @@ namespace TodoListAPI.Domain.Command
     public class CreateTodoCommand
     {
         [JsonIgnore]
-        public string Id { get; set; }
+        public string Id { get; set; } = Guid.NewGuid().ToString();
         public string UserId { get; set; }
         public string Description { get; set; }
         [JsonIgnore]
-        public string CreatedAt { get; set; }
+        public string CreatedAt { get; set; } = DateTime.UtcNow.ToUniversalTime().ToString("u").Replace(" ", "T");
 
         public static explicit operator TodoEntity(CreateTodoCommand command)
         {
