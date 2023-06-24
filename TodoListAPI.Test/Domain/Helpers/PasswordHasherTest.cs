4c96b8997cfc6c0b28385dc9c8efc1bcb6ec11d2
Note45 <antonion.2048@gmail.com>
2023-06-24 15:14:41 -0300

    test: add tests to the PasswordHasher

---
+++ TodoListAPI.Test/Domain/Helpers/PasswordHasherTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using TodoListAPI.Domain.Helpers;
+
+namespace TodoListAPI.Test.Domain.Helpers
+{
+    public class PasswordHasherTest
+	{
+        [Fact(DisplayName = "Should be able to hash the password")]
+        public void ShouldReturnTheHashedPasswordWhenCallHasher()
+        {
+            PasswordHasher passwordHasher = new();
+
+            string passwordHashed = passwordHasher.Hash("Password");
+
+            Assert.IsType<string>(passwordHashed);
+        }
+
+        [Fact(DisplayName = "Should be able to test the hashed password")]
+        public void ShouldTestHashedPasswordWhenCheckHasher()
+        {
+            PasswordHasher passwordHasher = new();
+
+            string password = "password-to-test";
+            string passwordHashed = passwordHasher.Hash(password);
+            var (Verified, NeedsUpgrade) = passwordHasher.Check(passwordHashed, password);
+
+            Assert.True(Verified);
+            Assert.False(NeedsUpgrade);
+        }
+    }
+}
+
