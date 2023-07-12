24001b84b915dae1e13b391a05bc05b8e5e1c865
amelo <amelo@bankly.com.br>
2023-07-12 14:07:29 -0300

    feat: add TodoMapper

---
+++ TodoListAPI/Domain/Mappers/TodoMapper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using TodoListAPI.Domain.Entities;
+using TodoListAPI.Infra.Database.Models;
+
+namespace TodoListAPI.Domain.Mappers
+{
+    public static class TodoMapper
+    {
+        public static TodoEntity ToEntity(TodoData todoSaved)
+        {
+            TodoEntity todoFormated = new()
+            {
+                Id = todoSaved.Id,
+                UserId = todoSaved.UserId,
+                Description = todoSaved.Description,
+                CreatedAt = todoSaved.CreatedAt.ToString(),
+            };
+
+            return todoFormated;
+        }
+
+    }
+}
+
