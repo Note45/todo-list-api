28e1fa5e528fc41f183b45d12dea18546cb9bfe6
amelo <amelo@bankly.com.br>
2023-09-25 08:59:07 -0300

    feat: add userMapper

---
+++ TodoListAPI/Domain/Mappers/UserMapper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using TodoListAPI.Domain.Entities;
+using TodoListAPI.Infra.Database.Models;
+
+namespace TodoListAPI.Domain.Mappers
+{
+    public static class UserMapper
+    {
+        public static UserData toData(UserEntity userEntity)
+        {
+            UserData userFormated = new()
+            {
+                Id = userEntity.Id,
+                Name = userEntity.Name,
+                Email = userEntity.Email,
+                Password = userEntity.Password,
+                CreatedAt = DateTime.Parse(userEntity.CreatedAt),
+                UpdatedAt = DateTime.Parse(userEntity.UpdatedAt),
+            };
+
+            return userFormated;
+        }
+    }
+}
+
