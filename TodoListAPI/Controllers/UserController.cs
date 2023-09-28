f24681c477b8ee2f6e96c0647fad68440b8ea426
amelo <amelo@acesso.com>
2023-09-28 13:11:16 -0300

    feat: add UserController

---
+++ TodoListAPI/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TodoListAPI.Application.Services;
+using TodoListAPI.Domain.Command;
+using TodoListAPI.Domain.Entities;
+using TodoListAPI.Domain.Services;
+
+namespace TodoListAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController: ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost]
+        [Route("add")]
+        public async Task<UserEntity> PostUser([FromBody] CreateUserCommand command)
+        {
+            return await _userService.AddUserAsync((UserEntity)command);
+        }
+    }
+}
