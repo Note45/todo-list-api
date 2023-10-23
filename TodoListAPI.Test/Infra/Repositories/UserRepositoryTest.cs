a6ad14b8c5cdf4ac6846b91deab7b7d2439f32d5
amelo <amelo@bankly.com.br>
2023-10-23 08:33:33 -0300

    test: mocking db methods

--- TodoListAPI.Test/Infra/Repositories/UserRepositoryTest.cs
+++ TodoListAPI.Test/Infra/Repositories/UserRepositoryTest.cs
@@ -1,17 +1,35 @@
 ﻿using System;
+using System.Net.Sockets;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Moq;
 using TodoListAPI.Domain.Entities;
+using TodoListAPI.Domain.Mappers;
 using TodoListAPI.Infra.Database.Config;
+using TodoListAPI.Infra.Database.Models;
 using TodoListAPI.Infra.Repositories;
 
 namespace TodoListAPI.Test.Infra.Repositories
 {
     public class UserRepositoryTest
     {
+        public Mock<IConfigurationSection> _mockConfSection;
+        public Mock<IConfiguration> _mockConfiguration;
+
+        public UserRepositoryTest()
+        {
+            _mockConfSection = new Mock<IConfigurationSection>();
+            _mockConfSection.SetupGet(m => m[It.Is<string>(s => s == "WebApiDatabase")]).Returns("mock_value");
+
+            _mockConfiguration = new Mock<IConfiguration>();
+            _mockConfiguration.Setup(a => a.GetSection(It.Is<string>(s => s == "WebApiDatabase"))).Returns(_mockConfSection.Object);
+        }
+
+
         [Fact(DisplayName = "Should be able to add an user to the users list")]
         public async void ShouldReturnTheUserDataWhenAddUserAsync()
         {
-            var userContextMock = new Mock<DataContext>();
+            var userContextMock = new Mock<DataContext>(_mockConfiguration.Object);
             UserRepository userRepository = new UserRepository(userContextMock.Object);
             UserEntity userData = new UserEntity()
             {
@@ -31,9 +49,12 @@ namespace TodoListAPI.Test.Infra.Repositories
         [Fact(DisplayName = "Should be able to get an user from the users list")]
         public async void ShouldReturnTheUserDataWhenGetUserAsync()
         {
-            var userContextMock = new Mock<DataContext>();
+            var userContextMock = new Mock<DataContext>(_mockConfiguration.Object);
+            Mock<DbSet<UserData>> mockUsersSet = new();
+            userContextMock.SetupGet(_ => _.Users).Returns(mockUsersSet.Object);
             UserRepository userRepository = new UserRepository(userContextMock.Object);
-            UserEntity userData = new UserEntity()
+
+            UserEntity userEntity = new UserEntity()
             {
                 Id = "new-id-1",
                 Name = "User Test Name",
@@ -42,17 +63,19 @@ namespace TodoListAPI.Test.Infra.Repositories
                 CreatedAt = DateTime.Today.ToString(),
                 UpdatedAt = DateTime.Today.ToString(),
             };
+            UserData? userFormatedToData = UserMapper.ToData(userEntity);
 
-            await userRepository.AddUserAsync(userData);
-            var userSaved = await userRepository.GetUserById(userData.Id);
+            userContextMock.Setup(x => x.FindAsync(It.IsAny<System.Type>())).ReturnsAsync(userFormatedToData);
 
-            Assert.Equal(userData, userSaved);
+            var userSaved = await userRepository.GetUserById(userEntity.Id);
+
+            Assert.Equal(userEntity, userSaved);
         }
 
         [Fact(DisplayName = "Should be able to remove an user from the users list")]
         public async void ShouldRemoveTheUserDataWhenRemoveUserByIdAsync()
         {
-            var userContextMock = new Mock<DataContext>();
+            var userContextMock = new Mock<DataContext>(_mockConfiguration.Object);
             UserRepository userRepository = new UserRepository(userContextMock.Object);
             UserEntity userData = new UserEntity()
             {
@@ -89,7 +112,7 @@ namespace TodoListAPI.Test.Infra.Repositories
         [Fact(DisplayName = "Should be able to update an user from the users list")]
         public async void ShouldUpdateTheUserDataWhenUpdateUserByIdAsync()
         {
-            var userContextMock = new Mock<DataContext>();
+            var userContextMock = new Mock<DataContext>(_mockConfiguration.Object);
             UserRepository userRepository = new UserRepository(userContextMock.Object);
             UserEntity userData = new UserEntity()
             {
