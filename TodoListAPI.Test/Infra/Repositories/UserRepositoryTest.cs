5d038a78bd4aa858a9186d671d4e177ad8edaaf4
Antonio Lisbôa <antonion.2048@gmail.com>
2024-01-22 22:33:21 -0300

    tests: add unit tests to GetUserByEmail

--- TodoListAPI.Test/Infra/Repositories/UserRepositoryTest.cs
+++ TodoListAPI.Test/Infra/Repositories/UserRepositoryTest.cs
@@ -3,9 +3,11 @@ using System.Net.Sockets;
 using System.Reflection.Metadata;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using MockQueryable.Moq;
 using Moq;
 using TodoListAPI.Domain.Entities;
 using TodoListAPI.Domain.Mappers;
+using TodoListAPI.Infra;
 using TodoListAPI.Infra.Database.Config;
 using TodoListAPI.Infra.Database.Models;
 using TodoListAPI.Infra.Repositories;
@@ -25,10 +27,27 @@ namespace TodoListAPI.Test.Infra.Repositories
             var mockConfiguration = new Mock<IConfiguration>();
             mockConfiguration.Setup(a => a.GetSection(It.Is<string>(s => s == "WebApiDatabase"))).Returns(mockConfSection.Object);
 
-            _userContextMock = new Mock<DataContext>(mockConfiguration.Object);
-            Mock<DbSet<UserData>> mockUsersSet = new();
-            _userContextMock.SetupGet(_ => _.Users).Returns(mockUsersSet.Object);
-            _userRepository = new UserRepository(_userContextMock.Object);
+            var data = new List<UserData> {
+               new UserData {
+                    Id = "new-id-1",
+                    Name = "User Test Name",
+                    Email = "user@email.com",
+                    Password = "123password",
+                    CreatedAt = DateTime.Today,
+                    UpdatedAt = DateTime.Today,
+               }
+            }.AsQueryable().BuildMock();
+
+            Mock<DbSet<UserData>> mockUserSet = new();
+            mockUserSet.As<IQueryable<UserData>>().Setup(m => m.Provider).Returns(data.Provider);
+            mockUserSet.As<IQueryable<UserData>>().Setup(m => m.Expression).Returns(data.Expression);
+            mockUserSet.As<IQueryable<UserData>>().Setup(m => m.ElementType).Returns(data.ElementType);
+            mockUserSet.As<IQueryable<UserData>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
+            var usersContextMock = new Mock<DataContext>(mockConfiguration.Object);
+            usersContextMock.SetupGet(_ => _.Users).Returns(mockUserSet.Object);
+
+            _userContextMock = usersContextMock;
+            _userRepository = new UserRepository(usersContextMock.Object);
         }
 
 
@@ -67,6 +86,24 @@ namespace TodoListAPI.Test.Infra.Repositories
             _userContextMock.Setup(x => x.Users.FindAsync(userFormatedToData.Id)).ReturnsAsync(userFormatedToData);
 
             var userSaved = await _userRepository.GetUserById(userEntity.Id);
+
+            Assert.Equivalent(userEntity, userSaved);
+        }
+
+        [Fact(DisplayName = "Should be able to get an user from the users list by email")]
+        public async void ShouldReturnTheUserDataWhenGetUserByEmailAsync()
+        {
+            UserEntity userEntity = new UserEntity()
+            {
+                Id = "new-id-1",
+                Name = "User Test Name",
+                Email = "user@email.com",
+                Password = "123password",
+                CreatedAt = DateTime.Today.ToString(),
+                UpdatedAt = DateTime.Today.ToString(),
+            };
+
+            var userSaved = await _userRepository.GetUserByEmail("user@email.com");
 
             Assert.Equivalent(userEntity, userSaved);
         }
