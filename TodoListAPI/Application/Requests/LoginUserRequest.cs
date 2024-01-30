70fbe137d7de793e0743c17816a810602f04585d
Antonio Lisbôa <antonion.2048@gmail.com>
2024-01-29 22:03:44 -0300

    feat: add LoginUserRequest and command

---
+++ TodoListAPI/Application/Requests/LoginUserRequest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using TodoListAPI.Infra.Database.Models;
+
+namespace TodoListAPI.Application.Requests
+{
+    public class LoginUserRequest
+    {
+        [FromBody] public string password { get; set; } = string.Empty;
+        [FromRoute] public string email { get; set; } = string.Empty;
+
+        public static implicit operator LoginUserCommand(LoginUserRequest request)
+        {
+            if (request is null) return default;
+
+            return new LoginUserCommand
+            {
+                Password = request.password,
+                Email = request.email
+            };
+        }
+    }
+}
+
---
+++ TodoListAPI/Domain/Command/LoginUserCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TodoListAPI.Infra.Database.Models
+{
+    public class LoginUserCommand
+    {
+        public string? Email { get; set; }
+
+        public string? Password { get; set; }
+    }
+}
+
