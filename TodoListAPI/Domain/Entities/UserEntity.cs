8c5f2cd9ac7c054d0515a9a104f37a40350958a7
Antonio Lisbôa <antonion.2048@gmail.com>
2024-04-03 08:16:42 -0300

    chore: exclude fields from user create route return

--- TodoListAPI/Domain/Entities/UserEntity.cs
+++ TodoListAPI/Domain/Entities/UserEntity.cs
@@ -1,10 +1,14 @@
-﻿namespace TodoListAPI.Domain.Entities
+﻿using System.Text.Json.Serialization;
+
+namespace TodoListAPI.Domain.Entities
 {
     public class UserEntity
     {
+        [JsonIgnore]
         public string Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
+        [JsonIgnore]
         public string Password { get; set; }
         public string CreatedAt { get; set; }
         public string UpdatedAt { get; set; }
