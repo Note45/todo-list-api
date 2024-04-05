da5b2a0a6ea9c00f04c1dc91ce24f8fbf9e76da5
Antonio Lisbôa <antonion.2048@gmail.com>
2024-04-04 21:55:13 -0300

    tests: add tests to CreateTodoCommandValidator

---
+++ TodoListAPI.Test/Domain/Validations/CreateTodoCommandValidatorTest.cs
@@ -0,0 +1,59 @@
+using TodoListAPI.Domain.Command;
+using TodoListAPI.Domain.Validations;
+
+namespace TodoListAPI.Test.Domain.Validations;
+
+public class CreateTodoCommandValidatorTest
+{
+    public CreateTodoCommandValidator _validator;
+    
+    public CreateTodoCommandValidatorTest()
+    {
+        _validator = new CreateTodoCommandValidator();
+    }
+    
+    [Fact(DisplayName = "Should be able to validate the object")]
+    public void ShouldReturnOkInValidation()
+    {
+        var command = new CreateTodoCommand
+        {
+            UserId = "b6158b42-a89c-45ab-a3eb-b4753fe1bb9b",
+            Description = "teste@teste.com",
+        };
+
+        var result = _validator.Validate(command);
+        
+        Assert.True(result.IsValid);
+    }
+    
+    
+    [Fact(DisplayName = "Should return userId field error")]
+    public void ShouldReturnEmailErrorInValidation()
+    {
+        var command = new CreateTodoCommand
+        {
+            UserId = "",
+            Description = "Teste description",
+        };
+
+        var result = _validator.Validate(command);
+
+        Assert.False(result.IsValid);
+        Assert.Contains(result.Errors, failure => failure.PropertyName.Equals(nameof(command.UserId)));
+    }
+    
+    [Fact(DisplayName = "Should return description field error")]
+    public void ShouldReturnPasswordErrorInValidation()
+    {
+        var command = new CreateTodoCommand
+        {
+            UserId = "b6158b42-a89c-45ab-a3eb-b4753fe1bb9b",
+            Description = "",
+        };
+
+        var result = _validator.Validate(command);
+
+        Assert.False(result.IsValid);
+        Assert.Contains(result.Errors, failure => failure.PropertyName.Equals(nameof(command.Description)));
+    }
+}
